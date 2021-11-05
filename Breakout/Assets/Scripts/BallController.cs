21c805651663ee4f5df816e4234d42de2adc91f2
Evan Allan <evanallan1026@gmail.com>
2021-11-05 18:05:48 -0500

    Count the balls that are onscreen and kill them if they go off

--- Breakout/Assets/Scripts/BallController.cs
+++ Breakout/Assets/Scripts/BallController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour {
 
+    static int ballCount = 0;
+
     public Vector3 StartVelocity = new Vector3(2, 3, 0);
 
     // Start is called before the first frame update
     void Start() {
         GetComponent<Rigidbody2D>().velocity = StartVelocity;
+        ballCount += 1;
     }
 
     // Update is called once per frame
     void Update() {
 
+    }
+
+    void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+
+    void OnDestroy() {
+        ballCount -= 1;
+        Debug.Log("Lost ball, new count: " + ballCount);
     }
 }
